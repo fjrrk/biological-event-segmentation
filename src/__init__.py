8eb701aaa1b295f753aa12db4e6f537404c63ccc
Illogical Mango <Tahia.Warda@gmail.com>
2026-04-25 22:32:36 -0400

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from .models import EBNet
+
+from .hybrid_trainer import HybridOrchestrator
+
+from .optimizer import objective
+
+
+"""
+Biological Event Segmentation Package
+Project: Event-Boundary Detection / Event Cognition
+Description: Exposes the EBNet architecture and associated 
+             hybrid training and optimization engines.
+"""
+
+# Version 1.0. Initialized package exports for this repo.
